e8d3baf2fa1bb503c06f4edbfe13d766459ed3fe
Memorios <25999631+Memorios@users.noreply.github.com>
2025-11-30 14:32:07 +0330

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::ptr;
-use windows::Win32::UI::WindowsAndMessaging::*;
 use windows::Win32::Foundation::*;
-use windows::Win32::System::LibraryLoader::GetModuleHandleW;
+use windows::Win32::UI::WindowsAndMessaging::*;
 
 static mut MOUSE_HOOK: HHOOK = HHOOK(0);
 static mut KEYBOARD_HOOK: HHOOK = HHOOK(0);
@@ -23,12 +22,12 @@ unsafe extern "system" fn keyboard_proc(code: i32, wparam: WPARAM, lparam: LPARA
         let now = GetTickCount();
 
         if now < BLOCK_UNTIL {
-            if info.vkCode == VK_RETURN.0 || info.vkCode == VK_APPS.0 {
-                return LRESULT(1); // BLOCK fake Enter / Menu key
+            if info.vkCode == 0x0D || info.vkCode == 0x5D {  // 0x0D = Enter, 0x5D = AppsKey
+                return LRESULT(1);
             }
         }
 
-        if info.vkCode == VK_ESCAPE.0 {
+        if info.vkCode == 0x1B {  // ESC to quit
             PostQuitMessage(0);
         }
     }
@@ -37,22 +36,24 @@ unsafe extern "system" fn keyboard_proc(code: i32, wparam: WPARAM, lparam: LPARA
 
 fn main() {
     unsafe {
-        let instance = GetModuleHandleW(None).unwrap_or(HINSTANCE(0));
+        let instance = GetModuleHandleW(None).unwrap_or(HINSTANCE(ptr::null_mut()));
 
-        MOUSE_HOOK = SetWindowsHookExW(WH_MOUSE_LL, Some(mouse_proc), instance, 0).unwrap_or(HHOOK(0));
-        KEYBOARD_HOOK = SetWindowsHookExW(WH_KEYBOARD_LL, Some(keyboard_proc), instance, 0).unwrap_or(HHOOK(0));
+        MOUSE_HOOK = SetWindowsHookExW(WH_MOUSE_LL, Some(mouse_proc), instance, 0)
+            .unwrap_or(HHOOK(ptr::null_mut()));
+        KEYBOARD_HOOK = SetWindowsHookExW(WH_KEYBOARD_LL, Some(keyboard_proc), instance, 0)
+            .unwrap_or(HHOOK(ptr::null_mut()));
 
-        println!("Xiaomi TV fake-key blocker is RUNNING!");
-        println!("→ Left click & middle click now work perfectly in Moonlight/Artemis");
-        println!("→ Press ESC to quit");
+        println!("Xiaomi TV fake-key blocker is RUNNING");
+        println!("Left & middle click now work perfectly");
+        println!("Press ESC to quit");
 
         let mut msg = MSG::default();
-        while GetMessageW(&mut msg, HWND(0), 0, 0).0 > 0 {
+        while GetMessageW(&mut msg, HWND(ptr::null_mut()), 0, 0).0 > 0 {
             TranslateMessage(&msg);
             DispatchMessageW(&msg);
         }
 
-        if MOUSE_HOOK.0 != 0 { UnhookWindowsHookEx(MOUSE_HOOK); }
-        if KEYBOARD_HOOK.0 != 0 { UnhookWindowsHookEx(KEYBOARD_HOOK); }
+        if MOUSE_HOOK.0 != ptr::null_mut() { UnhookWindowsHookEx(MOUSE_HOOK); }
+        if KEYBOARD_HOOK.0 != ptr::null_mut() { UnhookWindowsHookEx(KEYBOARD_HOOK); }
     }
 }
